8877352bfc24c354d383536adc572f03d7493b9e
Max Daniel <maximodanielcambero@gmail.com>
2026-05-21 17:20:21 -0400

    fix: use variable for ec2 public key in github actions

--- infra/variables.tf
+++ infra/variables.tf
@@ -6,3 +6,9 @@ variable "domain_name" {
   description = "Your registered domain (e.g. scoutcloud.dev)"
   type        = string
 }
+
+variable "ec2_public_key" {
+  description = "Public key for EC2 key pair"
+  type        = string
+  default     = ""
+}
